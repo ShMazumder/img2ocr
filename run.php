37b3e313dd0d732ceaa911d768528ed769aed490
Shazzad Hossain Mazumder <shmazumder23@gmail.com>
2023-07-24 11:45:43 +0600

    Initial commit

---
+++ run.php
@@ -0,0 +1,60 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use thiagoalessio\TesseractOCR\TesseractOCR;
+
+$user_agent = getenv("HTTP_USER_AGENT");
+
+if (strpos($user_agent, "Win") !== FALSE)
+    $os = "Windows";
+elseif (strpos($user_agent, "Mac") !== FALSE)
+    $os = "Mac";
+
+$inputDir = "imgs";
+$outputDir = "output";
+
+$ocr = new TesseractOCR();
+if ($os === "Windows") {
+    $ocr->executable("C:\Program Files\Tesseract-OCR\\tesseract.exe");
+} elseif ($os === "Mac") {
+    $ocr->tempDir("/tmp");
+    $ocr->executable("/usr/local/bin/tesseract");
+}
+
+
+$list = scandir($inputDir);
+
+for ($fileIndex = 0; $fileIndex < count($list); $fileIndex++) {
+    # code...
+
+    // echo $list[$fileIndex];
+    // echo "<br/>";
+
+    $fileName = $list[$fileIndex];
+    $img_path = $inputDir . DIRECTORY_SEPARATOR . $fileName;
+
+    if (in_array($fileName, array('.', '..', '.DS_Store'))) {
+        continue;
+    }
+
+    if (!is_file($img_path)) {
+        continue;
+    }
+
+    $ocr->image($img_path);
+
+    // $ocr->configFile('pdf');
+    // $ocr->setOutputFile('output/IMG_0101.pdf');
+    $result = $ocr->run();
+
+    // echo $result;
+    $outputFileName = $outputDir . DIRECTORY_SEPARATOR . (explode('.', $fileName)[0]) . '.txt';
+
+    file_put_contents($outputFileName, $result);
+}
+
+// echo json_encode(array("result" => implode("<br/>", explode("\n", $result))));
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'thiagoalessio/tesseract_ocr' => array(
+            'pretty_version' => '2.12.0',
+            'version' => '2.12.0.0',
+            'reference' => '0f10bd7b02bdcba59c4fbd98fbd93a56f93b09b7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../thiagoalessio/tesseract_ocr',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
