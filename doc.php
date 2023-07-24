e0b5633f4eb99b4e71d10222305ba294dabc0728
Shazzad Hossain Mazumder <shmazumder23@gmail.com>
2023-07-24 15:12:42 +0600

    update

--- doc.php
+++ doc.php
@@ -7,13 +7,23 @@ require_once __DIR__ . '/bootstrap.php';
 $phpWord = new \PhpOffice\PhpWord\PhpWord();
 
 $section = $phpWord->addSection();
+$phpWord->setDefaultFontName('Times New Roman');
+$phpWord->setDefaultFontSize(10);
 
-$fontStyle = new \PhpOffice\PhpWord\Style\Font();
-$fontStyle->setBold(true);
-$fontStyle->setName('Tahoma');
-$fontStyle->setSize(13);
+// $fontStyle = new \PhpOffice\PhpWord\Style\Font();
+// $fontStyle->setBold(true);
+// $fontStyle->setName('Tahoma');
+// $fontStyle->setSize(13);
 $myTextElement = $section->addText('"Believe you can and you\'re halfway there." (Theodor Roosevelt)');
-$myTextElement->setFontStyle($fontStyle);
+// $myTextElement->setFontStyle($fontStyle);
+
+$dirname = uniqid('', true);
+// $dir = \PhpOffice\PhpWord\Settings::getTempDir() . '/' . $dirname;
+$dir = __DIR__ . DIRECTORY_SEPARATOR . "tmp";
+if (!file_exists($dir)) {
+    mkdir($dir, 0777, true);
+}
+\PhpOffice\PhpWord\Settings::setTempDir($dir);
 
 // Saving the document as OOXML file...
 $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
