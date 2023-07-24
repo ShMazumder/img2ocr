62e67c65facbfee4a10b7932f56de8bc6a42de8c
Shazzad Hossain Mazumder <shmazumder23@gmail.com>
2023-07-24 15:17:39 +0600

    Update doc.php

--- doc.php
+++ doc.php
@@ -6,17 +6,48 @@ require_once __DIR__ . '/bootstrap.php';
 // Creating the new document...
 $phpWord = new \PhpOffice\PhpWord\PhpWord();
 
-$section = $phpWord->addSection();
-$phpWord->setDefaultFontName('Times New Roman');
-$phpWord->setDefaultFontSize(10);
+// each page 
 
-// $fontStyle = new \PhpOffice\PhpWord\Style\Font();
-// $fontStyle->setBold(true);
-// $fontStyle->setName('Tahoma');
-// $fontStyle->setSize(13);
-$myTextElement = $section->addText('"Believe you can and you\'re halfway there." (Theodor Roosevelt)');
-// $myTextElement->setFontStyle($fontStyle);
 
+
+$inputDir = "output";
+$outputDir = "output_doc";
+
+$list = scandir($inputDir);
+$chapterCount = 0;
+
+for ($fileIndex = 1; $fileIndex <= count($list); $fileIndex++) {
+    $fileName = $list[$fileIndex - 1];
+    $txt_path = $inputDir . DIRECTORY_SEPARATOR . $fileName;
+
+    if (in_array($fileName, array('.', '..', '.DS_Store'))) {
+        continue;
+    }
+
+    if (!is_file($txt_path)) {
+        continue;
+    }
+
+    if (is_dir($txt_path)) {
+        continue;
+    }
+
+
+    $section = $phpWord->addSection();
+    $phpWord->setDefaultFontName('Times New Roman');
+    $phpWord->setDefaultFontSize(10);
+
+    // $fontStyle = new \PhpOffice\PhpWord\Style\Font();
+    // $fontStyle->setBold(true);
+    // $fontStyle->setName('Tahoma');
+    // $fontStyle->setSize(13);
+    $content = file_get_contents($txt_path);
+    $myTextElement = $section->addText($content);
+    // $myTextElement->setFontStyle($fontStyle);
+    $section->addPageBreak();
+}
+
+// save 
 $dirname = uniqid('', true);
 // $dir = \PhpOffice\PhpWord\Settings::getTempDir() . '/' . $dirname;
 $dir = __DIR__ . DIRECTORY_SEPARATOR . "tmp";
@@ -28,5 +59,6 @@ if (!file_exists($dir)) {
 // Saving the document as OOXML file...
 $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
 
-$fileName = 'output_doc/helloWorld.docx';
-$objWriter->save($fileName);
+$fileName = "";
+$fileLocation = "output_doc/$fileName.docx";
+$objWriter->save($fileLocation);
