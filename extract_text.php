7423a335003369405b3f049904c74ed7435543e1
Shazzad Hossain Mazumder <shmazumder23@gmail.com>
2023-11-13 03:42:06 +0600

    up

---
+++ extract_text.php
@@ -0,0 +1,113 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use thiagoalessio\TesseractOCR\TesseractOCR;
+
+$user_agent = getenv("HTTP_USER_AGENT");
+
+if (strpos($user_agent, "Win") !== FALSE)
+    $os = "Windows";
+elseif (strpos($user_agent, "Mac") !== FALSE)
+    $os = "Mac";
+
+$ocr = new TesseractOCR();
+$ocr->lang('ben', 'eng');
+$langs = $ocr->availableLanguages();
+// var_dump($langs);
+
+if ($os === "Windows") {
+    $ocr->executable("C:\Program Files\Tesseract-OCR\\tesseract.exe");
+} elseif ($os === "Mac") {
+    $ocr->tempDir("/tmp");
+    $ocr->executable("/usr/local/bin/tesseract");
+}
+
+$inputDirRoot = $target_dir; //"uploads";
+$outputDirRoot = "results/" . explode("/", $target_dir)[1];
+if (!file_exists($outputDirRoot)) {
+    mkdir($outputDirRoot, 0777, true);
+}
+
+// echo json_encode(array("result" => implode("<br/>", explode("\n", $result))));
+$listOfInputDir = scandir($inputDirRoot);
+
+for ($inputDirIndex = 0; $inputDirIndex < count($listOfInputDir); $inputDirIndex++) {
+
+    $currentItem = $listOfInputDir[$inputDirIndex];
+    $inputDir = $inputDirRoot . DIRECTORY_SEPARATOR . $currentItem;
+    $outputDir = $outputDirRoot . DIRECTORY_SEPARATOR . $currentItem;
+
+    if (in_array($currentItem, array('.', '..', '.DS_Store'))) {
+        continue;
+    }
+
+    if (!is_dir($inputDir)) {
+        echo "Skipped: " . $inputDir;
+        continue;
+    }
+
+    processDir($ocr, $inputDir, $outputDir);
+}
+
+function processDir($ocr, $inputDir, $outputDir)
+{
+    $removeFirstList = false;
+
+    if (!file_exists($outputDir)) {
+        mkdir($outputDir, 0777, true);
+    }
+
+    $list = scandir($inputDir);
+
+    for ($fileIndex = 0; $fileIndex < count($list); $fileIndex++) {
+        # code...
+
+        // echo $list[$fileIndex];
+        // echo "<br/>";
+
+        $fileName = $list[$fileIndex];
+        $img_path = $inputDir . DIRECTORY_SEPARATOR . $fileName;
+
+        if (in_array($fileName, array('.', '..', '.DS_Store'))) {
+            continue;
+        }
+
+        if (!is_file($img_path)) {
+            continue;
+        }
+
+        $ocr->image($img_path);
+
+        // $ocr->configFile('pdf');
+        // $ocr->setOutputFile('output/IMG_0101.pdf');
+        try {
+            $result = $ocr->run();
+        } catch (\Throwable $th) {
+            echo $img_path . "=Failed (" . $th->getMessage() . ")";
+            continue;
+        }
+
+        // echo $result;
+        $fileNameArr = explode('.', $fileName);
+        $fileNameArr = array_slice($fileNameArr, 0, count($fileNameArr) - 1);
+        $outputFileName = $outputDir . DIRECTORY_SEPARATOR . implode($fileNameArr) . '.txt';
+
+        // file_put_contents($outputFileName, $result); // full save
+
+        // save without first line and removing single breaks.
+        $contents = explode("\n", $result); // break the result by newline: array
+
+        if ($removeFirstList) {
+            $contents = array_slice($contents, 1); // remove first line : array
+        }
+
+        $contents = implode(" ", $contents); // join the array using space : string
+        $contents = explode("  ", $contents); // break content using double space : array
+        $contents = implode(" ", $contents); // join the array with break
+
+
+        file_put_contents($outputFileName, $contents);
+
+        echo $outputFileName . "=OK" . "<br/>";
+    }
+}
--- index.php
+++ index.php
@@ -23,38 +23,75 @@ if (strcasecmp($action, "OCR") === 0) {
     require "extract_phone.php";
 } else if (strcasecmp($action, "VIEW") === 0) {
     require "view.php";
+} else if (strcasecmp($action, "IMAGE2TEXT") === 0 || (isset($_POST['action']) && strcasecmp($_POST['action'], "IMAGE2TEXT")===0)) {
+
+    if (!isset($_FILES["fileToUpload"])) {
+        // var_dump($_GET);
+        var_dump($_FILES);
+        exit();
+    }
+    if ($_FILES["fileToUpload"]["error"] > 0)
+        echo "Error: " . $_FILES["fileToUpload"]["error"] . "<br />";
+    else {
+        echo "Upload: " . $_FILES["fileToUpload"]["name"] . "<br />";
+        echo "Type: " . $_FILES["fileToUpload"]["type"] . "<br />";
+        echo "Size: " . ($_FILES["fileToUpload"]["size"] / 1024) . " Kb<br />";
+        echo "Stored in: " . $_FILES["fileToUpload"]["tmp_name"];
+
+        $target_dir = "uploads/".date('Y-m-d H-i-s')."";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        if(!file_exists($target_dir)){
+            mkdir($target_dir, 0777, true);
+        }
+
+        move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+
+        require "extract_text.php";
+    }
 } else {
     // upload options?
 
-    echo "<div style='width:100%; display:flex; justify-content:around;'>";
+    // echo "<div style='width:100%; display:flex; justify-content:around;'>";
 
-    echo "<div style='width: 200px'>";
-    echo "<a href='index.php?action=ocr'>OCR<a>";
-    echo "</div>";
+    // echo "<div style='width: 200px'>";
+    // echo "<a href='index.php?action=ocr'>OCR<a>";
+    // echo "</div>";
 
-    echo "<div style='width: 200px'>";
-    echo "<a href='index.php?action=pdf'>PDF<a>";
-    echo "</div>";
+    // echo "<div style='width: 200px'>";
+    // echo "<a href='index.php?action=pdf'>PDF<a>";
+    // echo "</div>";
 
-    echo "<div style='width: 200px'>";
-    echo "<a href='index.php?action=doc'>DOC<a>";
-    echo "</div>";
+    // echo "<div style='width: 200px'>";
+    // echo "<a href='index.php?action=doc'>DOC<a>";
+    // echo "</div>";
 
-    echo "<div style='width: 200px'>";
-    echo "<a href='index.php?action=multidoc'>Multi DOC<a>";
-    echo "</div>";
+    // echo "<div style='width: 200px'>";
+    // echo "<a href='index.php?action=multidoc'>Multi DOC<a>";
+    // echo "</div>";
 
-    echo "<div style='width: 200px'>";
-    echo "<a href='index.php?action=doc2pdf'>Doc To PDF<a>";
-    echo "</div>";
+    // echo "<div style='width: 200px'>";
+    // echo "<a href='index.php?action=doc2pdf'>Doc To PDF<a>";
+    // echo "</div>";
 
-    echo "<div style='width: 200px'>";
-    echo "<a href='index.php?action=extract_phone'>Extract Phone<a>";
-    echo "</div>";
+    // echo "<div style='width: 200px'>";
+    // echo "<a href='index.php?action=extract_phone'>Extract Phone<a>";
+    // echo "</div>";
 
-    echo "<div style='width: 200px'>";
-    echo "<a href='index.php?action=view'>VIEW<a>";
-    echo "</div>";
+    // echo "<div style='width: 200px'>";
+    // echo "<a href='index.php?action=view'>VIEW<a>";
+    // echo "</div>";
 
-    echo "</div>";
+    // echo "</div>";
+
+    // 
+
+    // echo "<form action='index.php' method='post' enctype='multipart/form-data'>";
+    // echo "<input name='fileToUpload' id='fileToUpload' type='file' required>";
+    // echo "<input type='submit' name='action' value='IMAGE2TEXT'/>";
+    // echo "</form>";
+
+    echo '<form action="index.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" name="action" value="IMAGE2TEXT"/>
+        </form>';
 }
